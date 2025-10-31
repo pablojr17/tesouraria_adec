54448ef1e1471ae160067cbd5204572126786f75
Pablo <pablojr17@mail.com>
2025-10-31 17:01:55 -0300

    update bd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,7 +65,7 @@ atualizarSaldo() {
 
   this.totalEntradas = this.entradas.reduce((acc, e) => acc + e.valor, 0);
   this.totalSaidas = this.saidas.reduce((acc, s) => acc + s.valor, 0);
-  this.saldo = (this.totalEntradas + 148.66) - this.totalSaidas;
+  this.saldo = (this.totalEntradas + 365.11) - this.totalSaidas;
 }
 
   atualizarSaidas(): void {
